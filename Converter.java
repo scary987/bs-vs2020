088c2122a520513336093cfb4b6f64d277db50ef
Tobias Reincke <tobias.reincke@uni-rostock.de>
2020-05-29 23:05:02 +0200

    finished xdr,cdr translation

--- Converter.java
+++ Converter.java
@@ -6,11 +6,43 @@ import java.time.Year;
 import java.util.Date;
 import java.util.*;
 import java.lang.reflect.Array;  
-public class Converter {
+public class Converter
 
+ {
+	static long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
 	
-	
-	
+	public static byte[] cdr( Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		byte month =(byte) (cal.get(Calendar.MONTH)+1);
+		int year = cal.get(Calendar.YEAR);
+		//int month = Year.now().
+		byte day = (byte) cal.get(Calendar.DAY_OF_MONTH);
+		//String time = ""+year+month+day+'\0';
+		Date now = cal.getTime();
+		long timePortion = now.getTime() % MILLIS_PER_DAY;
+		String time = ""+year+"//"+month+"//"+day+"//"+timePortion+'\0';
+		//System.out.printf("%d%d%d%d\n",year,month,day,timePortion);
+	//	System.out.printf(time);
+	//	System.out.println("\n");
+	//	System.out.println(time.length());
+		byte[] bytes = ByteBuffer.allocate(4).putInt(time.length()).array();
+
+		/*for (byte b : bytes) {
+   			System.out.format("0x%x ", b);
+		}*/
+		byte[] a;
+		
+		a = time.getBytes();
+		//System.out.println(a+" "+bytes);
+		byte[] c = concat(bytes,a);
+		
+		//for(byte b:c)System.out.println(b);
+		return c;
+	}
+	public static byte[] xdr() {
+		return xdr(new Date());
+	}
 	public static byte[] xdr( Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
@@ -18,39 +50,88 @@ public class Converter {
 		int year = cal.get(Calendar.YEAR);
 		//int month = Year.now().
 		byte day = (byte) cal.get(Calendar.DAY_OF_MONTH);
-		String time = ""+year+month+day+'\0';
-		
-		System.out.printf("%d%d%d\n",year,month,day);
-		System.out.printf(time);
+		//String time = ""+year+month+day+'\0';
+		Date now = cal.getTime();
+		long timePortion = now.getTime() % MILLIS_PER_DAY;
+		String time = ""+year+"//"+month+"//"+day+"//"+timePortion+'\0';
+		//System.out.printf("%d%d%d%d\n",year,month,day,timePortion);
+	//	System.out.printf(time);
+	//	System.out.println("\n");
+	//	System.out.println(time.length());
 		byte[] bytes = ByteBuffer.allocate(4).putInt(time.length()).array();
 
-		for (byte b : bytes) {
+		/*for (byte b : bytes) {
    			System.out.format("0x%x ", b);
-		}
+		}*/
 		byte[] a;
-		a = time.getBytes();
-		byte[] c = new byte[a.length + bytes.length];
-		System.arraycopy(a, 0, c, 0, a.length);
-		System.arraycopy(bytes, 0, c, a.length, bytes.length);
 		
-		System.out.println(c);
+		a = time.getBytes();
+		//System.out.println(a+" "+bytes);
+		byte[] c = concat(bytes,a);
+		if (c.length % 4 != 0) {
+			a = new byte[c.length%4];
+			for(byte byt:a) {
+				byt=0;
+			}
+			return concat(c,a);
+		}
+		
+		//for(byte b:c)System.out.println(b);
 		return c;
 	}
-	public static String xdrtostring(byte[] s ) {
-			int x= 0;
-		for(int i = 0 ; i<17; i++) {
-			x+=Math.pow(s[i],17-i );
-		}
+	
+	public static String xcdrtostring(byte[] b ) {
 		
+		int a = byteArrayToInt(b);
+		//System.out.println("This is a: "+a);
+		byte[] c = new byte[b.length-4];
+		System.arraycopy(b, 4, c, 0, a);
 		
-		return "";
+		return new String(c);
 	}
 	
-	public static byte[] xdr() {
-		return xdr (new Date());
+	public static  byte[] concat(byte[] first, byte[] second) {
+		  byte[] result = Arrays.copyOf(first, first.length + second.length);
+		  System.arraycopy(second, 0, result, first.length, second.length);
+		  return result;
+		}
+	public static int byteArrayToInt(byte[] b) 
+	{
+		//Big Endian in both cases
+	    return   b[3] & 0xFF |
+	            (b[2] & 0xFF) << 8 |
+	            (b[1] & 0xFF) << 16 |
+	            (b[0] & 0xFF) << 24;
+	}
+
+	public static byte[] intToByteArray(int a)
+	{
+	    return new byte[] {
+	        (byte) ((a >> 24) & 0xFF),
+	        (byte) ((a >> 16) & 0xFF),   
+	        (byte) ((a >> 8) & 0xFF),   
+	        (byte) (a & 0xFF)
+	    };
+	}
+	public static byte[] cdr() {
+		return cdr (new Date());
 	}
 	public static void main(String[] args) {
-		byte[] b =xdr();
+		Date date = new Date();
+		byte[] cdr =cdr(date);
+		byte[] xdr = xdr(date);
+		//System.out.println("This is a: "+a);
+		//byte[] c = new byte[b.length-4];
+		System.out.print("CDR: ");
+		for(byte c:cdr)System.out.print(c+" ");
+		System.out.print("\n");
+		
+		System.out.print("XDR: ");
+		for(byte c:xdr)System.out.print(c+" ");
+		System.out.print("\n");
+		System.out.println(xcdrtostring(cdr));
+		System.out.println(xcdrtostring(xdr));
+		
 	}
 
 }
